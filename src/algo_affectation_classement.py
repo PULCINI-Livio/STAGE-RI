7c9b237e6b490a3dd2a6e4c478280a4cbcabbcc1
PULCINI-Livio <pulcinilivio04@gmail.com>
2025-05-22 10:21:32 +0200

    Update algo_affectation_classement.py
    ajout de la recherche des universités compatibles avec la spécialité
    Le générateur du df etudiant fictif prejnd en compte également les spé pour le choix des universités

--- src/algo_affectation_classement.py
+++ src/algo_affectation_classement.py
@@ -1,9 +1,9 @@
 import logging
+import random
 import pandas as pd
 import numpy as np
 
 from excel_en_dataframe import charger_excels
-from generateur_donnees_fictives import generer_df_choix_etudiants
 from conversion_df_brute import conversion_df_brute_pour_affectation
 
 # Configuration de base du logging
@@ -13,6 +13,54 @@ logging.basicConfig(
     format='%(asctime)s - %(levelname)s - %(message)s',
     level=logging.INFO                 # Niveau minimal des messages (DEBUG, INFO, WARNING, ERROR, CRITICAL)
 )
+
+def generer_df_choix_etudiants_spe_compatible(n, df_univ):
+    """
+    Génère un DataFrame contenant les choix d'université de n étudiants pour trois semestres. On prend en compte la spécialités des univ et des étudiants.
+
+    Paramètres :
+    ------------
+    n : int
+        Le nombre d'étudiants (et donc de lignes du DataFrame).
+
+    Retour :
+    --------
+    DataFrame pandas avec les colonnes :
+        - Id Etudiant : identifiant numérique (1 à n)
+        - Choix S8 : 5 noms d'univ
+        - Choix S9 : idem
+        - Choix S10 : idem
+
+    Remarques :
+    -----------
+    - Les codes Univ proviennent de la liste fixe ['AAAA', ..., 'ZZZZ'] (26 codes possibles).
+    - Aucun doublon n'est présent dans une même ligne de choix.
+    - L'ordre des codes dans chaque choix est aléatoire.
+    """
+    if n < 1:
+        raise ValueError("Le nombre d'étudiants doit être au moins 1.")
+    
+    liste_semestre = ["S8", "S9", "S10"]
+    liste_specialites = ["MM", "MC", "MMT", "SNI", "BAT", "EIT", "IDU", "ESB", "AM"]
+    
+    
+    data = {
+        "Id Etudiant": [],
+        "Specialite": [],
+        "Choix S8": [],
+        "Choix S9": [],
+        "Choix S10": []
+    }
+
+    for i in range(1, n + 1):
+        data["Id Etudiant"].append(i)
+        specialite_choisie = random.choice(liste_specialites)
+        data["Specialite"].append(specialite_choisie)
+        for semestre in liste_semestre:
+            liste_univ_compatibles = get_liste_univ_compatible(df_univ, semestre, specialite_choisie)
+            choix = random.sample(liste_univ_compatibles, 5)  # 5 noms différents, ordre aléatoire
+            data["Choix " + str(semestre)].append(", ".join(choix))
+    return pd.DataFrame(data)
 
 def get_nombre_places_total(df:pd.DataFrame, nom_du_partenaire:str, semestre:str):
     """Retourne le nombre de places pour l'université correspondante.
@@ -124,7 +172,7 @@ def convertir_colonne_en_tuple(df:pd.DataFrame, colonne:str):
     """
     df[colonne] = df[colonne].apply(lambda x: tuple(map(str.strip, x.split(","))) if pd.notna(x) else x)
 
-def get_universite_la_moins_remplie(df_univ:pd.DataFrame, choix:tuple, semestre:str, calcul_completion:str="Taux") -> str:
+def get_universite_la_moins_remplie(df_univ:pd.DataFrame, choix:tuple|list, semestre:str, calcul_completion:str="Taux") -> str:
     """Retourne un str correspondant à l'université la moins remplie selon la méthode de calcul 
     à un seul choix pour les semestres qu'il a choisi selon un scénario hybride entre le classement et la complétion des partenaires.
     
@@ -142,10 +190,45 @@ def get_universite_la_moins_remplie(df_univ:pd.DataFrame, choix:tuple, semestre:
     i = 1
     while i < len(choix):
         courant = choix[i]
-        if get_taux_completion_places(df_univ, courant, semestre) < get_taux_completion_places(df_univ, univ_la_moins_rempli, semestre):
-            univ_la_moins_rempli = courant
+        if calcul_completion == "Taux":
+            if get_taux_completion_places(df_univ, courant, semestre) < get_taux_completion_places(df_univ, univ_la_moins_rempli, semestre):
+                univ_la_moins_rempli = courant
+        else:
+            if get_nombre_places_prises(df_univ, courant, semestre) < get_nombre_places_prises(df_univ, univ_la_moins_rempli, semestre):
+                univ_la_moins_rempli = courant
         i += 1
     return univ_la_moins_rempli
+
+def get_liste_univ_compatible(df_univ:pd.DataFrame, semestre:str, specialite:str):
+    """Retourne la liste des universités qui sont compatibles avec la spécialité et le semestre en paramètre.
+    
+    Args:
+        df_univ: Le dataframe des universités partenaires.
+        semestre: Le semestre choisi en str.
+
+    Returns:
+        res: la liste des universités qui sont compatibles avec la spécialité et le semestre en paramètre.
+    """
+    
+    res = []
+    for idx, row in df_univ.iterrows(): # On parcours chaque ligne du df
+        if specialite in row["Specialites Compatibles " + str(semestre)]: # Si l'univ est compatible 
+            res.append(row["NOM DU PARTENAIRE"])
+    return res
+
+def get_univ_compatible_la_moins_remplie(df_univ:pd.DataFrame, semestre:str, specialite:str, calcul_completion:str="Taux"):
+    """Retourne le nom de l'université qui est compatible avec la spécialité en paramètre et qui est la moins remplie.
+    
+    Args:
+        df_univ: Le dataframe des universités partenaires.
+        semestre: Le semestre choisi en str.
+
+    Returns:
+        res: le nom de l'université qui est compatible avec la spécialité en paramètre et qui est la moins remplie.
+    """
+    
+    liste_univ_compatibles = get_liste_univ_compatible(df_univ, semestre, specialite)
+    return get_universite_la_moins_remplie(df_univ, liste_univ_compatibles, semestre, calcul_completion)
 
 def traitement_scenario_hybride(df_univ:pd.DataFrame, df_etudiants:pd.DataFrame, limite_ordre:int=0, calcul_completion:str="Taux"):
     """Retourne un df correspondant aux affectations de chaque étudiant 
@@ -200,8 +283,8 @@ def traitement_scenario_hybride(df_univ:pd.DataFrame, df_etudiants:pd.DataFrame,
                         attribution_faite = True
 
                 elif not attribution_faite: # Si tout les choix ont été rejeté
-                    specialite = get_specialite()
-                    univ_compatible_la_moins_remplie = get_univ_compatible_la_moins_remplie()
+                    specialite = row["Specialite"]
+                    univ_compatible_la_moins_remplie = get_univ_compatible_la_moins_remplie(df_univ, semestre, specialite, calcul_completion)
                     if place_est_disponible(df_univ, univ_compatible_la_moins_remplie, semestre):
                         choix_final = choix_courant
                         attribution_faite = True    
@@ -225,8 +308,8 @@ if test:
     #print(dataframes.keys())
     df_univ = conversion_df_brute_pour_affectation(dataframes)["universites_partenaires"]
     print(df_univ)
-    df_etu_fictif = generer_df_choix_etudiants(400, df_univ["NOM DU PARTENAIRE"].tolist())
-    #print(df_etu_fictif)
-
-    print(traitement_scenario_hybride(df_univ, df_etu_fictif, 3, "Taux" ))
+    df_etu_fictif = generer_df_choix_etudiants_spe_compatible(20, df_univ)
+    print(df_etu_fictif)
+    #df_etu_fictif.to_excel("df_etu_fictif.xlsx") 
+    #print(traitement_scenario_hybride(df_univ, df_etu_fictif, 3, "Taux" ))
     #print(df_univ)

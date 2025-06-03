ea132479efec9ecd5804fc74166924034bba8e89
PULCINI-Livio <pulcinilivio04@gmail.com>
2025-06-03 11:49:52 +0200

    Update simulation.py

--- src/main/python/simulation.py
+++ src/main/python/simulation.py
@@ -1,16 +1,17 @@
 import time
 
-from algo_affectation_classement import traitement_scenario_hybride, generer_df_choix_etudiants_spe_compatible, comparaison_taux_completion_prio_non_prio 
+from algo_affectation_classement import traitement_scenario_hybride, generer_df_choix_etudiants_spe_compatible, comparaison_taux_completion_prio_non_prio, tri_df_etudiant_semestre_ponderation 
 from conversion_df_brute import conversion_df_brute_pour_affectation
 from excel_en_dataframe import charger_excels
 
 dataframes = charger_excels("src\\main\\data")
 df_univ = conversion_df_brute_pour_affectation(dataframes)["universites_partenaires"]
 
-#df_etu_fictif = generer_df_choix_etudiants_spe_compatible(160, df_univ)
-df_etu_fictif = conversion_df_brute_pour_affectation(dataframes)["choix_etudiants"]
+df_etu_fictif = generer_df_choix_etudiants_spe_compatible(200, df_univ, proba_un_seul_semestre=0.2)
+#df_etu_fictif = conversion_df_brute_pour_affectation(dataframes)["choix_etudiants"]
 
 df_etu_fictif.to_excel("src\\main\\output\\df_etu_fictif.xlsx", index=False)
+df_etu_fictif = tri_df_etudiant_semestre_ponderation(df_etu_fictif, alpha=0.1)
 df_final = traitement_scenario_hybride(df_univ, df_etu_fictif, limite_ordre=1)
 df_univ.to_excel("src\\main\\output\\df_univ.xlsx", index=False)
 df_final.to_excel("src\\main\\output\\df_etu_fictif_final.xlsx", index=False)
